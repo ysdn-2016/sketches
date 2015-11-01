41f54016ca78478b32ded3ed42d7c9359957d2e0
Nate Butler <iamnbutler@gmail.com>
2015-11-01 01:08:09 -0500

    need to fix janky positioning

--- js/plugins/fg.js
+++ js/plugins/fg.js
@@ -1,35 +1,27 @@
 $(document).ready(function(){
-
-	var items = $("#selected-work-fg").find('li');
 	
 	$(".a-u-fg").on( 'click', '.fg-left', function(){
-		console.log("clicked");
-		for (i = 0; i < items.length; i++) { 
-			var item = items.eq(i);
-			var pos = item.offset();
-			item.offset({ left : pos.left + 1050 });
-			console.log(pos.left);
-		}
-		items.removeClass("focused");
-		$(this).next().addClass("fg-right");
+		var w = $(this).width();
+		var container = $(this).parents(".a-u-fg-inner");
+		var pos = container.position();
+		$(this).next().removeClass("focused").addClass("fg-right");
 		$(this).prev().addClass("fg-left");
 		$(this).addClass("focused")
 					 .removeClass("fg-left");
+		container.offset({ left: pos.left + w });
+		console.log(w);
 		return false;
 	});
 
 	$(".a-u-fg").on( 'click', '.fg-right', function(){
-		console.log("clicked");
-		for (i = 0; i < items.length; i++) { 
-			var item = items.eq(i);
-			var pos = item.offset();
-			item.offset({ left : pos.left - 1050 });
-		}
-		items.removeClass("focused");
+		var w = $(this).width();
+		var container = $(this).parents(".a-u-fg-inner");
+		var pos = container.position();
 		$(this).next().addClass("fg-right");
-		$(this).prev().addClass("fg-left");
+		$(this).prev().removeClass("focused").addClass("fg-left");
 		$(this).addClass("focused")
 					 .removeClass("fg-right");
+		container.offset({ left: pos.left - w });
 		return false;
 	});
 });
